4907e7904e5a61cd6ec823d360146627fe93b75b
tiborkosa <tkosa.web@gmail.com>
2021-02-22 09:41:08 +0200

    started the collection preview page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 import SHOP_DATA from './shop.data';
-
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 class ShopPage extends React.Component {
 
     constructor(props){
         super(props);
 
         this.state = {
-            shopData : SHOP_DATA
+            collections : SHOP_DATA
         }
     }
 
     render(){
+        const {collections} = this.state;
         return (
-            <div>Shop page</div>
+            <div className='shop-page'>
+                {
+                    collections.map( ( {id , ...otherCollectionProps} ) => (
+                        <CollectionPreview key={id} {...otherCollectionProps}/>
+                    ))
+                }
+            </div>
         )
     }
 }
