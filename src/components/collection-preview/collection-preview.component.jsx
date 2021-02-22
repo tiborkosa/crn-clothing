049b74f969baa5cc2146228fab6172adcdc91df5
tiborkosa <tkosa.web@gmail.com>
2021-02-22 10:11:04 +0200

    added preview component and styled the preview page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import './collection-preview.style.scss';
+import  CollectionItem from '../collection-item/collection-item.component';
 
-const PreviewCollection = ({ title, items }) => (
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.map( item => (
-                    <div key={item.id}>{item.name}</div>
-                ))
+                items
+                    .filter( (item, idx) => idx < 4)
+                    .map(({ id, ...otherItemProps }) => (
+                        <CollectionItem key={id} {...otherItemProps} />
+                      ))
             }
         </div>
     </div>
 );
 
-export default PreviewCollection;
+export default CollectionPreview;
