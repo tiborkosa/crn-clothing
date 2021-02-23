79b12de43cb657f770111e1de57808835c90c68d
tiborkosa <tkosa.web@gmail.com>
2021-02-23 20:40:11 +0200

    Finishe google oAuth and added sign out functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './pages/sign-in-sign-up/signInSignUp.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInSignUp} />
-      </Switch>
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribedFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+ 
+  componentWillUnmount(){
+    console.log("componentWillUnmount [ App.js]");
+    if(this.unsubscribedFromAuth) {
+      this.unsubscribedFromAuth();
+      console.log('unmounted and unsubsrcibed')
+    }
+  }
+
+  render(){
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUp} />
+        </Switch>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAwrwUU3WOtnKOUFEd_t4XnZdLcCEj7bvk",
+    authDomain: "crn-db-a1ac0.firebaseapp.com",
+    projectId: "crn-db-a1ac0",
+    storageBucket: "crn-db-a1ac0.appspot.com",
+    messagingSenderId: "660342838402",
+    appId: "1:660342838402:web:1fb99224c2c52130be509b",
+    measurementId: "G-X93PD990PJ"
+  };
+
+  export default config;
